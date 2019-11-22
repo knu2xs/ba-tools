dec5c80b28ba2df8e8dd7ce363b3b104154d554f
Joel McCune <knu2xs@gmail.com>
2019-11-22 10:25:59 -0800

    Add beginning of tests for enrich

---
+++ testing/test_enrich.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+import sys
+
+import pandas as pd
+from sklearn.pipeline import Pipeline
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from ba_tools import enrich
+
+data_dir = Path(__file__).parent/'test_data'
+gdb = data_dir/'test_data.gdb'
+
+block_groups = gdb/'block_groups'
+block_group_id_field = 'ID'
+
+
+def test_enrich_all():
+    enrich_df = enrich.enrich_all(block_groups)
+    assert(isinstance(pd.DataFrame, enrich_df))
