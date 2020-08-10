a9d496c79e9fb9a2d2688a8659d5351ebf6cc4bc
Joel McCune <knu2xs@gmail.com>
2020-08-10 12:18:35 -0700

    Updates to be able to ingest wider range of layer types as inputs

--- make.cmd
+++ make.cmd
@@ -4,6 +4,7 @@
 
 SETLOCAL
 SET PROJECT_DIR=%cd%
+SET CONDA_PARENT=arcgispro-py3
 SET PROJECT_NAME=ba-tools
 SET ENV_NAME=ba-tools
 
@@ -13,6 +14,33 @@ SET ENV_NAME=ba-tools
 
 :: Jump to command
 GOTO %1
+
+:: Build the local environment from the environment file
+:env
+    ENDLOCAL & (
+
+        :: Run this from the ArcGIS Python Command Prompt
+        :: Clone and activate the new environment
+        CALL conda create --name "%ENV_NAME%" --clone "%CONDA_PARENT%" -y
+        CALL activate "%ENV_NAME%"
+
+        :: Install nodejs so it does not throw an error later
+        CALL conda install -y nodejs
+
+        :: Install additional packages
+        CALL conda env update -f environment.yml
+
+        :: Install the local package in development mode
+        CALL python -m pip install -e .
+
+        :: Additional steps for the map widget to work in Jupyter Lab
+        CALL jupyter labextension install @jupyter-widgets/jupyterlab-manager -y
+        CALL jupyter labextension install arcgis-map-ipywidget@1.8.2 -y
+
+        :: Set the ArcGIS Pro Python environment
+        proswap "%ENV_NAME%"
+    )
+    EXIT /B
 
 :: Update the current environment with resources needed to publish the package
 :env_dev
