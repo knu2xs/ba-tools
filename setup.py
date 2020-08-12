c7e1c9f1eb9f502250ef1d6c92ab7b0c66af143a
Joel McCune <knu2xs@gmail.com>
2020-08-12 15:29:41 -0700

    Bump version: 0.8.4 → 0.9.0-dev0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme:
 
 setuptools.setup(
     name='ba-tools',
-    version='0.8.4',
+    version='0.9.0-dev0',
     author='Joel McCune',
     author_email='jmccune@esri.com',
     description='Feature engineering using ArcGIS Pro with Business Analyst for using quantitative Geography '
